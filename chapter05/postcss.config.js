e35618fcd5330b42e9df2bb085bb77f146647b12
Dori <981926536@qq.com>
2026-03-10 01:11:51 +0800

    fix: chapter05添加cssnano实现CSS压缩

--- chapter05/postcss.config.js
+++ chapter05/postcss.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  plugins: [
-    require('postcss-preset-env')
-  ]
-}
+module.exports = (ctx) => {
+  return {
+    plugins: [
+      require('postcss-preset-env'),
+      // 仅在生产环境启用 CSS 压缩
+      ctx.env === 'production' ? require('cssnano')({ preset: 'default' }) : false,
+    ].filter(Boolean),
+  };
+};
